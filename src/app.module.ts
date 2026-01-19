d9369a813a85219b76b291fdf45ad877a90f9d8b
SmasBalloon <mathissauvage24000@gmail.com>
2026-01-19 11:56:08 +0100

    feat: updated app module to include necessary imports and exports.

---
+++ src/app.module.ts
@@ -0,0 +1,11 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { PrismaModule } from './prisma/prisma.module';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
+
+@Module({
+    imports: [PrismaModule, AuthModule, UsersModule],
+    controllers: [AppController],
+})
+export class AppModule { }
